52f6261cd42c8590ae36239ff356f58289480915
SWAPNIL SHUKLA <SWAPNIL.SHUKLA@servernp.com>
2018-10-12 12:04:56 +0530

    DB COnfig

--- config/db.js
+++ config/db.js
@@ -1,2 +1,4 @@
-module.exports.CONNECTION_URI = "mongodb://localhost:27017/tech-post";
+module.exports.CONNECTION_URI = "mongodb://swapnil8074:swapnil8074@ds231133.mlab.com:31133/tech-post";
+// module.exports.CONNECTION_URI = "mongodb://localhost:27017/tech-post";
+
 
